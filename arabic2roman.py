9dda4a19d10d43c56091e3eec6df1cd5eb115647
Suli <sulihu@bu.edu>
2020-01-26 23:01:22 -0500

    Fulfill linter requirements

--- arabic2roman.py
+++ arabic2roman.py
@@ -6,12 +6,13 @@ def arabic2roman(num):
     Transform integer to Roman #.
     Only works for 0 - 10,000.
     """
-    M = ["", "M", "MM", "MMM"]
-    C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-    X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-    I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    roman_m = ["", "M", "MM", "MMM"]
+    roman_c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    roman_x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    roman_i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
     try:
-        return M[num//1000] + C[(num%1000)//100] + X[(num%100)//10] + I[num%10]
+        return roman_m[num//1000] + roman_c[(num % 1000)//100] + \
+            roman_x[(num % 100)//10] + roman_i[num % 10]
     except Exception:
         return "Number too large."
 
@@ -20,6 +21,6 @@ def main():
     print(arabic2roman(8))
     print(arabic2roman(0))
 
+
 if __name__ == "__main__":
     main()
-
